a88ae26b96f33ffd5ea905293cca961c91b420f7
CR <37802179+chossrutter@users.noreply.github.com>
2019-06-17 12:10:33 +0100

    Troubleshooting git commit hook issue with python executable

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 #from setuptools import find_packages
 from setuptools import setup
 
-setup()
+setup(name='pre_commit_checks',
+      entry_points = {
+        'console_scripts': ['secrets_filecontent=secrets_filecontent:main', 'secrets_filename=secrets_filename:main'$
+      }
+      )
+
