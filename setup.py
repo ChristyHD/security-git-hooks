55fd5ec076f548bd0736f109dc6b7ccbfe8572e5
CR <37802179+chossrutter@users.noreply.github.com>
2019-06-19 14:33:50 +0100

    TESTING

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='pre_commit_checks',
       entry_points = {
-        'console_scripts': ['secrets_filecontent=secrets_filecontent:main', 'secrets_filename=secrets_filename:main'$
+        'console_scripts': ['secrets_filecontent=secrets_filecontent:main', 'secrets_filename=secrets_filename:main']
       }
       )
 
