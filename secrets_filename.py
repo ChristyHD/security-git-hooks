3525c35081158c523f2bd0393802aa5d40e93be4
CR <37802179+chossrutter@users.noreply.github.com>
2019-06-19 14:31:28 +0100

    Testing automatic hooks update using pre-commit

--- secrets_filename.py
+++ secrets_filename.py
@@ -3,45 +3,27 @@
 import argparse
 import sys
 import re
-from conf import _FILE_NAME_REGEXES
 
-"""Parse the files in a github commit for potentially sensitive filenames, per rules 
-defined at https://github.com/hmrc/app-config-base/blob/master/leak-detection.conf"""
+IGNORE_KEYWORD = "leak-detection-ignore"
 
-
-for regex in _FILE_NAME_REGEXES:
+for regex in _FILE_CONTENT_REGEXES:
     try:
-        re.compile(_FILE_NAME_REGEXES[regex])
+        re.compile(_FILE_CONTENT_REGEXES[regex])
     except:
         raise
 
 
-def detect_match_against_filename(files_to_check):
-    """checks argument against compiled regexes"""
-    for rule, regex in _FILE_NAME_REGEXES.items():
-        if re.search(regex, files_to_check):
-            return regex
+def detect_secret_in_line(line_to_check):
+    """compiles regex and checks against line."""
+    for rule, regex in _FILE_CONTENT_REGEXES.items():
+        if re.search(re.compile(regex), line_to_check):
+            return rule
 
 
 def main(argv=None):
-    """Parses filenames and provides outut.
-    Note that if manually passed a directory as argument, checks are not recursive as Git 
-    adds files to a commit individually."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filenames", nargs="*", help="Files to check")
-    args = parser.parse_args(argv)
-    exit_code = 0
+    print("UPDATED")
 
-    for filename in args.filenames:
-        match = detect_match_against_filename(filename)
-        if match:
-            exit_code = 1
-            print(
-                "{file} may contain sensitive information due to the file type".format(
-                    file=filename
-                )
-            )
-    return exit_code
+    return 1
 
 
 if __name__ == "__main__":
